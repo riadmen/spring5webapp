a1053a385aacc7223aab7d3ee056e514ed91f71f
riadmen <riad.mennour@owiin.com>
2022-07-11 19:34:12 +0100

    Adding thymeleaf MVC

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -17,6 +17,6 @@ public class BookController {
     @RequestMapping("/books")
     public String getBooks(Model model){
         model.addAttribute("books", bookRepository.findAll());
-        return "Books";
+        return "Books/list";
     }
 }
--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -78,7 +78,6 @@ public class Author {
                 "id=" + id +
                 ", fistName='" + fistName + '\'' +
                 ", lastName='" + lastName + '\'' +
-                ", books=" + books +
                 '}';
     }
 }
